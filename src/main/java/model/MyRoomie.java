cfd3eeb32c1dda0385e548679ff0380dbd9e118d
parkchoeun <chdms7471@naver.com>
2021-11-05 01:19:45 +0900

    tmp

--- src/main/java/model/MyRoomie.java
+++ src/main/java/model/MyRoomie.java
@@ -1,2 +1,32 @@
-package model;public class MyRoomie {
+package model;
+
+public class MyRoomie {
+    private String studentId;
+    private String roomieId;
+
+    public MyRoomie() {}
+    public MyRoomie(String studentId, String roomieId) {
+        this.studentId = studentId;
+        this.roomieId = roomieId;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getRoomieId() {
+        return roomieId;
+    }
+
+    public void setRoomieId(String roomieId) {
+        this.roomieId = roomieId;
+    }
+
+    public boolean isPicked(String studentId) {
+        return this.roomieId.equals(studentId);
+    }
 }
