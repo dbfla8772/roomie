8493ab13e9a46a961553096b5ff2f155dd93c4ac
parkchoeun <chdms7471@naver.com>
2021-11-08 00:24:36 +0900

    temp

--- src/main/java/model/MyRoomie.java
+++ src/main/java/model/MyRoomie.java
@@ -1,32 +1,39 @@
 package model;
 
 public class MyRoomie {
+    private int m_id;  //myroomie table의 고유id
     private int s_id;
-    private int roomie_id;
+    private int roomie_id;  //myroomie로 추가한 학생의 id
 
     public MyRoomie() {}
-    public MyRoomie(int s_id, int roomie_id) {
+
+    public MyRoomie(int m_id, int s_id, int roomie_id) {
+        this.m_id = m_id;
         this.s_id = s_id;
         this.roomie_id = roomie_id;
     }
 
-    public int getStudent_id() {
+    public int getM_id() {
+        return m_id;
+    }
+
+    public void setM_id(int m_id) {
+        this.m_id = m_id;
+    }
+
+    public int getS_id() {
         return s_id;
     }
 
-    public void setStudent_id(int s_id) {
+    public void setS_id(int s_id) {
         this.s_id = s_id;
     }
 
-    public int getRoomie_id() { return roomie_id;}
+    public int getRoomie_id() {
+        return roomie_id;
+    }
 
     public void setRoomie_id(int roomie_id) {
         this.roomie_id = roomie_id;
-    }
-
-    public boolean isPicked(int s_id) {
-        if (s_id == roomie_id)
-            return true;
-        return false;
     }
 }
