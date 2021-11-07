b2e3124ba9f24f5ef3ca1adff9fe538fdc587ad9
dbfla8772 <dbfla5527@naver.com>
2021-11-08 01:29:24 +0900

    Point DAO

--- src/main/java/model/dao/PointDAO.java
+++ src/main/java/model/dao/PointDAO.java
@@ -13,9 +13,8 @@ public class PointDAO {
     }
 
     public Point create(Point point) throws SQLException {
-        String sql = "INSERT INTO point VALUES (Idseq.nextval, ?, ?, ?)";
-        Object[] param = new Object[] {point.getPoint_binary(), point.getPoint_dec(),
-                point.getS_id()};
+        String sql = "INSERT INTO point VALUES (Idseq.nextval, ?, ?)";
+        Object[] param = new Object[] {point.getPoint_binary(), point.getPoint_dec()};
         jdbcUtil.setSqlAndParameters(sql, param);	// JDBCUtil 에 insert문과 매개 변수 설정
 
         String key[] = {"pt_id"};	// PK 컬럼의 이름
@@ -24,7 +23,7 @@ public class PointDAO {
             ResultSet rs = jdbcUtil.getGeneratedKeys();
             if(rs.next()) {
                 int generatedKey = rs.getInt(1);   // 생성된 PK 값
-                point.setPt_id(generatedKey);
+                point.setS_id(generatedKey);
             }
             return point;
         } catch (Exception ex) {
@@ -39,12 +38,10 @@ public class PointDAO {
 
     public int update(Point point) throws SQLException {
         String sql = "UPDATE point "
-                + "SET cName=?, descr=?, chairId=? "
-                + "WHERE cId=?";
-        String chairId = comm.getChairId();
-        if (chairId.equals("")) chairId = null;
-        Object[] param = new Object[] {comm.getName(), comm.getDescription(),
-                chairId, comm.getId()};
+                + "SET point_binary=?, point_dec=? "
+                + "WHERE s_id=?";
+
+        Object[] param = new Object[] { point.getPoint_binary(), point.getPoint_dec()};
         jdbcUtil.setSqlAndParameters(sql, param);	// JDBCUtil에 update문과 매개 변수 설정
 
         try {
@@ -61,30 +58,25 @@ public class PointDAO {
         return 0;
     }
 
-    public Point findPoint(Point point) throws SQLException {
-        String sql = "";
-        jdbcUtil.setSqlAndParameters(sql, new Object[] {userId});	// JDBCUtil에 query문과 매개 변수 설정
+    public Point findPoint(int sId) throws SQLException {
+        String sql = "SELECT s_id, point_binary, point_dec " +
+                "FROM point " + "WHERE s_id=?";
+        jdbcUtil.setSqlAndParameters(sql, new Object[]{sId});    // JDBCUtil에 query문과 매개 변수 설정
 
+       Point point = null;
         try {
-            ResultSet rs = jdbcUtil.executeQuery();		// query 실행
-            if (rs.next()) {						// 학생 정보 발견
-                User user = new User(		// User 객체를 생성하여 학생 정보를 저장
-                        userId,
-                        rs.getString("password"),
-                        rs.getString("name"),
-                        rs.getString("email"),
-                        rs.getString("phone"),
-                        rs.getInt("commId"),
-                        rs.getString("cName"));
-                return user;
+            ResultSet rs = jdbcUtil.executeQuery();        // query 실행
+            if (rs.next()) {                        // 학생 정보 발견
+                point = new Point(        // Community 객체를 생성하여 커뮤니티 정보를 저장
+                        rs.getInt("s_id"),
+                        rs.getLong("point_binary"),
+                        rs.getInt("point_dec"));
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
-            jdbcUtil.close();		// resource 반환
+            jdbcUtil.close();        // resource 반환
         }
-        return null;
+        return point;
     }
-
-
 }
