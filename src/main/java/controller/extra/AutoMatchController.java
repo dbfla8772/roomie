f93a89cda9403e142c18e3c45ae552421b0c3cd7
KwonMinJi00 <alswlghgk@naver.com>
2021-11-09 22:52:03 +0900

    controller commit

--- src/main/java/controller/extra/AutoMatchController.java
+++ src/main/java/controller/extra/AutoMatchController.java
@@ -1,5 +1,15 @@
 package controller.extra;
 
-public class AutoMatchController {
+import controller.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AutoMatchController implements Controller {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        return "/profile/autoMatch.jsp";
+    }
 
 }
