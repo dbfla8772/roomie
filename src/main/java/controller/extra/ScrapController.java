ea11819b86cbe453b2711bcb715edbd87bbc944b
kimehhk <kimehhk@gmail.com>
2021-11-10 02:06:54 +0900

    setting & jsp

--- src/main/java/controller/extra/ScrapController.java
+++ src/main/java/controller/extra/ScrapController.java
@@ -1,5 +1,14 @@
 package controller.extra;
 
-public class ScrapController {
+import controller.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ScrapController implements Controller {
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return null;
+    }
 }
