f38e2c61e153f299aef643b0e73d8f0695115df7
parkchoeun <chdms7471@naver.com>
2021-11-27 00:16:31 +0900

    deleteMail

---
+++ src/main/java/controller/extra/DeleteMailController.java
@@ -0,0 +1,39 @@
+package controller.extra;
+
+import controller.Controller;
+import controller.user.UserSessionUtils;
+import model.Profile;
+import model.service.MailManager;
+import model.service.ProfileManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class DeleteMailController implements Controller {
+    private static final Logger log = LoggerFactory.getLogger(DeleteMailController.class);
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // 로그인 여부 확인
+        if (!UserSessionUtils.hasLogined(request.getSession())) {
+            return "redirect:/student/login";		// login form 요청으로 redirect
+        }
+        HttpSession session = request.getSession();
+        int s_id = (int) session.getAttribute(UserSessionUtils.USER_SESSION_ID);
+        MailManager mailManager = MailManager.getInstance();
+        int ch_id = Integer.parseInt(request.getParameter("ch_id"));
+        int flag = Integer.parseInt(request.getParameter("flag"));
+
+        log.debug("s_id 확인: " + s_id + ", ch_id 확인: " + ch_id + ", flag 확인: " + flag);
+
+        //GET
+        mailManager.remove(ch_id);
+
+        if (flag == 0) {
+            return "/mail/receive/receiveList.jsp";
+        }
+        return "/mail/send/sendForm.jsp";
+    }
+}
